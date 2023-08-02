541ba98a81f7a5637aa76dfec978013ae8b6192b
Evgeniy-Nevskiy <122367665+Evgeniy-Nevskiy@users.noreply.github.com>
2023-08-03 02:04:39 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,154 +1,62 @@
 #include "stat_reader.h"
 
-namespace transport_catalogue
-{
-    namespace detail
-    {
-        namespace bus
-        {
+#include <iomanip>
 
-            void show_terminal(BusQueryResult bus_info, std::ostream &output)
-            {
-                if (bus_info.not_found)
-                {
-                    output << "Bus " << bus_info.name << ": not found" << std::endl;
-                }
-                else
-                {
-                    output << "Bus " << bus_info.name << ": "
-                           << bus_info.stops_on_route << " stops on route, "
-                           << bus_info.unique_stops << " unique stops, "
-                           << bus_info.route_length << " route length, "
-                           << std::setprecision(6) << bus_info.curvature
-                           << " curvature" << std::endl;
-                }
+namespace transport {
+
+    void ShowRequests(std::istream& in, TransportCatalogue& catalogue, std::ostream& out) {
+        size_t requests_num;
+        in >> requests_num;
+        for (size_t i = 0; i < requests_num; ++i) {
+            std::string keyword, str;
+            in >> keyword;
+            std::getline(in, str);
+            if (keyword == "Bus") {
+                ShowBus(str, catalogue, out);
             }
-            BusQueryResult query(TransportCatalogue &catalogue, std::string_view bus_name)
-            {
-                BusQueryResult bus_info;
-                auto entry = 4;
-                bus_name = bus_name.substr(entry);
-
-                Bus *bus = catalogue.get_bus(bus_name);
-                if (bus != nullptr)
-                {
-                    bus_info.name = bus->name;
-                    bus_info.not_found = false;
-                    bus_info.stops_on_route = bus->stops.size();
-                    bus_info.unique_stops = catalogue.get_uniq_stops(bus).size();
-                    bus_info.route_length = bus->route_length;
-                    bus_info.curvature = double(catalogue.get_distance_to_bus(bus) / catalogue.get_length(bus));
-                }
-                else
-                {
-                    bus_info.name = bus_name;
-                    bus_info.not_found = true;
-                }
-                return bus_info;
-            }
-
-        }   // end namespace bus
-
-        namespace stop
-        {
-
-            void show_terminal(const StopQueryResult &stop_info, std::ostream &output)
-            {
-                if (stop_info.not_found)
-                {
-                    output << "Stop " << stop_info.name << ": not found" << std::endl;
-                }
-                else
-                {
-                    if (stop_info.buses_name.size() == 0)
-                    {
-                        output << "Stop " << stop_info.name << ": no buses" << std::endl;
-                    }
-                    else
-                    {
-                        output << "Stop " << stop_info.name << ": buses ";
-
-                        for (std::string_view bus_name : stop_info.buses_name)
-                        {
-                            output << bus_name;
-                            output << " ";
-                        }
-                        output << std::endl;
-                    }
-                }
-            }
-
-            StopQueryResult query(TransportCatalogue &catalogue, std::string_view stop_name)
-            {
-                auto entry = 5;
-                stop_name = stop_name.substr(entry);
-                std::unordered_set<const Bus *> unique_buses;
-
-                StopQueryResult stop_info;
-
-                Stop *stop = catalogue.get_stop(stop_name);
-
-                if (stop != NULL)
-                {
-                    stop_info.name = stop->name;
-                    stop_info.not_found = false;
-                    unique_buses = catalogue.stop_get_uniq_buses(stop);
-
-                    if (unique_buses.size() > 0)
-                    {
-                        for (const Bus *bus : unique_buses)
-                        {
-                            stop_info.buses_name.push_back(bus->name);
-                        }
-                        std::sort(stop_info.buses_name.begin(), stop_info.buses_name.end());
-                    }
-                }
-                else
-                {
-                    stop_info.name = stop_name;
-                    stop_info.not_found = true;
-                }
-                return stop_info;
-            }
-
-        }   // end namespace stop
-
-        void execute_query(TransportCatalogue &catalogue, std::string_view str)
-        {
-            if (str.substr(0, 3) == "Bus")
-            {
-                bus::show_terminal(bus::query(catalogue, str), std::cout);
-            }
-            else if (str.substr(0, 4) == "Stop")
-            {
-                stop::show_terminal(stop::query(catalogue, str), std::cout);
-            }
-            else
-            {
-                std::cout << "Error query" << std::endl;
+            if (keyword == "Stop") {
+                ShowStop(str, catalogue, out);
             }
         }
+    }
 
-        void parse_and_execute_queries(std::istream &input, TransportCatalogue &catalogue)
+    void ShowBus(std::string& line, TransportCatalogue& catalogue, std::ostream& out) {
+        std::string bus_name = line.substr(1, line.npos);
+
+        auto it = catalogue.SearchBus(bus_name);
+        if (it != nullptr)
         {
-            std::string count;
-            std::getline(input, count);
+            out << "Bus " << it->bus_name << ": " << it->stops_count
+                << " stops on route, " << it->unique_stops_count << " unique stops, " << it->marshrut_length
+                << " route length, " << it->curvature << " curvature" << std::endl;
+        }
+     
+        else
+        {
+            out << "Bus " << bus_name << ": not found" << std::endl;
+        }
+       
+    }
 
-            std::string str;
-            std::vector<std::string> query;
-            auto amount = stoi(count);
-
-            for (int i = 0; i < amount; ++i)
-            {
-                std::getline(input, str);
-                query.push_back(str);
+    void ShowStop(std::string& line, TransportCatalogue& catalogue, std::ostream& out) {
+        std::string stop_name = line.substr(1, line.npos);
+        if (catalogue.SearchStop(stop_name)) {
+            out << "Stop " << stop_name << ": ";
+            auto buses = catalogue.GetBusesByStop(stop_name);
+            if (!buses.empty()) {
+                out << "buses ";
+                for (const auto& bus : buses) {
+                    out << bus << " ";
+                }
+                out << "\n";
             }
-
-            for (auto &strr : query)
-            {
-                execute_query(catalogue, strr);
+            else {
+                out << "no buses\n";
             }
         }
+        else {
+            out << "Stop " << stop_name << ": not found\n";
+        }
+    }
 
-    }   // end namespace detail
-}       // end namespace transport_catalogue
+} // namespace transport
