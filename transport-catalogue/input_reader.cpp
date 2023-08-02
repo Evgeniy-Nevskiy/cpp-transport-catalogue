4d96cb250ad8d7d52b219705f31ed07f367b0be5
Evgeniy-Nevskiy <122367665+Evgeniy-Nevskiy@users.noreply.github.com>
2023-08-03 02:02:52 +0300

    Update input_reader.cpp

--- transport-catalogue/input_reader.cpp
+++ transport-catalogue/input_reader.cpp
@@ -1,170 +1,109 @@
 #include "input_reader.h"
-#include "stat_reader.h"
-#include <algorithm>
 
-namespace transport_catalogue
-{
-    namespace detail
-    {
-        namespace stop
-        {
+namespace transport {
 
-            Stop parse_stop(const std::string &str)
-            {
-                auto twopoint_pos = str.find(':');
-                auto comma_pos = str.find(',');
-                auto entry_length = 5;
-                auto distance = 2;
-                Stop stop;
+    void Parsing(std::istream& in, TransportCatalogue& catalogue) {
+        std::vector<std::string> buses;
+        std::vector<std::string> stops;
+        std::vector<std::string> distances;
+        size_t size;
 
-                stop.name = str.substr(entry_length,
-                                       twopoint_pos - entry_length);
-                stop.lat = stod(str.substr(twopoint_pos + distance,
-                                                comma_pos - twopoint_pos - distance));
-                stop.lng = stod(str.substr(comma_pos + distance));
-
-                return stop;
+        in >> size;
+        for (size_t i = 0; i < size; ++i) {
+            std::string word, str;
+            in >> word;
+            std::getline(in, str);
+            if (word == "Stop") {
+                stops.push_back(str);
             }
-
-        }   // end namespace stop
-
-        namespace distance
-        {
-
-            std::vector<Distance> parse_distances(const std::string &str, TransportCatalogue &catalogue)
-            {
-                std::vector<Distance> distances;
-                auto entry_length = 5;
-                auto twopoint_pos = str.find(':');
-                auto space = 2;
-
-                std::string str_copy = str;
-                std::string name = str_copy.substr(entry_length,
-                                                   twopoint_pos - entry_length);
-                str_copy = str_copy.substr(str_copy.find(',') + 1);
-                str_copy = str_copy.substr(str_copy.find(',') + space);
-
-                while (str_copy.find(',') != std::string::npos)
-                {
-                    int distance = stoi(str_copy.substr(0, str_copy.find('m')));
-                    std::string stop_dist_name = str_copy.substr(str_copy.find('m') + entry_length);
-                    stop_dist_name = stop_dist_name.substr(0, stop_dist_name.find(','));
-                    distances.push_back({catalogue.get_stop(name),
-                                         catalogue.get_stop(stop_dist_name), distance});
-                    str_copy = str_copy.substr(str_copy.find(',') + space);
-                }
-                std::string last_name = str_copy.substr(str_copy.find('m') + entry_length);
-                int distance = stoi(str_copy.substr(0, str_copy.find('m')));
-                distances.push_back({catalogue.get_stop(name), catalogue.get_stop(last_name), distance});
-
-                return distances;
+            if (word == "Bus") {
+                buses.push_back(str);
             }
+        }
+        distances = stops;
+        for (auto& stop : stops) {
+            auto [stop_name, crds] = ParsingStop(stop);
+            catalogue.AddStop(stop_name, crds);
+        }
+        for (auto& stop : distances) {
+            AddDistances(stop, catalogue);
+        }
+        for (auto& bus : buses) {
+            auto [bus_name, marshrut, crug] = ParsingBus(bus, catalogue);
+            catalogue.AddBus(bus_name, marshrut, crug);
+            bus = {};
+        }
+    }
 
-        }   // end namespace distance
+    std::pair<std::string, geo::Coordinates> ParsingStop(std::string& str) {
+        std::string stop_name = str.substr(1, str.find_first_of(':') - str.find_first_of(' ') - 1);
+        double latitude = std::stod(str.substr(str.find_first_of(':') + 2, str.find_first_of(',') - 1));
+        double longitude;
+        str.erase(0, str.find_first_of(',') + 2);
+        if (str.find_last_of(',') == str.npos) {
+            longitude = std::stod(str.substr(0, str.npos - 1));
+            str.clear();
+        }
+        else {
+            longitude = std::stod(str.substr(0, str.find_first_of(',')));
+            str.erase(0, str.find_first_of(',') + 2);
+        }
+        geo::Coordinates stop_coordinates = { latitude, longitude };
 
-        namespace bus
-        {
+        return { stop_name, stop_coordinates };
+    }
 
-            Bus parse_bus(std::string_view str, TransportCatalogue &catalogue)
-            {
-                auto entry_length = 4;
-                auto distance = 2;
-                auto twopoint_pos = str.find(':');
-                Bus bus;
-                bus.name = str.substr(entry_length,
-                                      twopoint_pos - entry_length);
+    void AddDistances(std::string& str, TransportCatalogue& catalogue) {
+        if (!str.empty()) {
+            std::string stopname_begin = ParsingStop(str).first;
+            const Stop* first = catalogue.SearchStop(stopname_begin);
 
-                str = str.substr(twopoint_pos + distance);
-
-                auto more_pos = str.find('>');
-                if (more_pos == std::string_view::npos)
-                {
-                    auto tire_pos = str.find('-');
-
-                    while (tire_pos != std::string_view::npos)
-                    {
-                        bus.stops.push_back(catalogue.get_stop(str.substr(0, tire_pos - 1)));
-
-                        str = str.substr(tire_pos + distance);
-                        tire_pos = str.find('-');
+            while (!str.empty()) {
+                int lenght = 0;
+                std::string stopname_end;
+                lenght = std::stoi(str.substr(0, str.find_first_of("m to ")));
+                str.erase(0, str.find_first_of("m to ") + 5);
+                if (str.find("m to ") == str.npos) {
+                    stopname_end = str.substr(0, str.npos - 1);
+                    const Stop* second = catalogue.SearchStop(stopname_end);
+                    catalogue.SetDistance(first, second, lenght);
+                    if (!catalogue.GetDistance(first, second)) {
+                        catalogue.SetDistance(second, first, lenght);
                     }
-
-                    bus.stops.push_back(catalogue.get_stop(str.substr(0, tire_pos - 1)));
-                    size_t size = bus.stops.size() - 1;
-
-                    for (size_t i = size; i > 0; i--)
-                    {
-                        bus.stops.push_back(bus.stops[i - 1]);
+                    str.clear();
+                }
+                else {
+                    stopname_end = str.substr(0, str.find_first_of(','));
+                    const Stop* second = catalogue.SearchStop(stopname_end);
+                    catalogue.SetDistance(first, second, lenght);
+                    if (!catalogue.GetDistance(first, second)) {
+                        catalogue.SetDistance(second, first, lenght);
                     }
-                }
-                else
-                {
-                    while (more_pos != std::string_view::npos)
-                    {
-                        bus.stops.push_back(catalogue.get_stop(str.substr(0, more_pos - 1)));
-
-                        str = str.substr(more_pos + distance);
-                        more_pos = str.find('>');
-                    }
-
-                    bus.stops.push_back(catalogue.get_stop(str.substr(0, more_pos - 1)));
-                }
-
-                return bus;
-            }
-
-        }   // end namespace bus
-
-        void fill_catalogue(std::istream &input, TransportCatalogue &catalogue)
-        {
-
-            std::string count;
-            std::getline(input, count);
-
-            if (count != "")
-            {
-                std::string str;
-                std::vector<std::string> buses;
-                std::vector<std::string> stops;
-                int amount = stoi(count);
-                auto bus_distance = 3;
-
-                for (int i = 0; i < amount; ++i)
-                {
-                    std::getline(input, str);
-
-                    if (str != "")
-                    {
-                        auto space_pos = str.find_first_not_of(' ');
-                        str = str.substr(space_pos);
-
-                        if (str.substr(0, bus_distance) != "Bus")
-                        {
-                            stops.push_back(str);
-                        }
-                        else
-                        {
-                            buses.push_back(str);
-                        }
-                    }
-                }
-
-                for (auto stop : stops)
-                {
-                    catalogue.add_stop(stop::parse_stop(stop));
-                }
-
-                for (auto stop : stops)
-                {
-                    catalogue.add_distance(distance::parse_distances(stop, catalogue));
-                }
-
-                for (auto bus : buses)
-                {
-                    catalogue.add_bus(bus::parse_bus(bus, catalogue));
+                    str.erase(0, str.find_first_of(',') + 2);
                 }
             }
         }
+    }
 
-    }   // end namespace detail
-}       // end namespace transport_catalogue
+    std::tuple<std::string, std::vector<const Stop*>, bool> ParsingBus(std::string& str, transport::TransportCatalogue& catalogue) {
+        std::vector<const Stop*> marshrut_stops;
+        std::string bus_name = str.substr(1, str.find_first_of(':') - 1);
+        str.erase(0, str.find_first_of(':') + 2);
+        bool crug = false;
+        std::string stop_name;
+        auto pos = str.find('>') != str.npos ? '>' : '-';
+        while (str.find(pos) != str.npos) {
+            stop_name = str.substr(0, str.find_first_of(pos) - 1);
+            const Stop* pointer_stop = catalogue.SearchStop(stop_name);
+            marshrut_stops.push_back(pointer_stop);
+            str.erase(0, str.find_first_of(pos) + 2);
+        }
+        stop_name = str.substr(0, str.npos - 1);
+        const Stop* pointer_stop = catalogue.SearchStop(stop_name);
+        marshrut_stops.push_back(pointer_stop);
+        if (pos == '>') crug = true;
+
+        return { bus_name, marshrut_stops, crug };
+    }
+
+} // namespace transport
