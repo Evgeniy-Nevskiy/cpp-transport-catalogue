05d969601df21ecf07a654d3b5ba901b70fb7e8f
Evgeniy-Nevskiy <122367665+Evgeniy-Nevskiy@users.noreply.github.com>
2023-08-04 22:42:42 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,14 +1,1 @@
-#pragma once
 
-#include "transport_catalogue.h"
-
-#include <iostream>
-
-namespace transport {
-
-	void Parsing(std::istream& in, TransportCatalogue& catalogue);
-	std::pair<std::string, geo::Coordinates> ParsingStop(std::string& line);
-	void AddDistances(std::string& line, TransportCatalogue& catalogue);
-	std::tuple<std::string, std::vector<const Stop*>, bool> ParsingBus(std::string& line, transport::TransportCatalogue& catalogue);
-
-} // namespace transport
