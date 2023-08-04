ca1c8fa7169f40af357a2bdc8d67f784f5891a0d
Evgeniy-Nevskiy <122367665+Evgeniy-Nevskiy@users.noreply.github.com>
2023-08-04 22:47:24 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,11 +1,1 @@
-#pragma once
 
-#include "transport_catalogue.h"
-
-namespace transport {
-
-	void ShowRequests(std::istream& in, TransportCatalogue& catalogue, std::ostream& out);
-	void ShowBus(std::string& line, TransportCatalogue& catalogue, std::ostream& out);
-	void ShowStop(std::string& line, TransportCatalogue& catalogue, std::ostream& out);
-
-} // namespace transport
