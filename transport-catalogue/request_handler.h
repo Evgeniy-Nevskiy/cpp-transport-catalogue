1e278b83d03789333df9c0cd5f87046c72b8568f
Evgeniy-Nevskiy <122367665+Evgeniy-Nevskiy@users.noreply.github.com>
2023-08-04 22:30:28 +0300

    Create request_handler.h

---
+++ transport-catalogue/request_handler.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "json.h"
+#include "transport_catalogue.h"
+#include "map_renderer.h"
+
+#include <sstream>
+
+class RequestHandler {
+public:
+    RequestHandler(const transport::TransportCatalogue& catalogue, const renderer::MapRenderer& renderer)
+        : catalogue_(catalogue)
+        , renderer_(renderer)
+    {
+    }
+
+
+    const transport::Bus* GetBus(const std::string_view bus_number) const;
+
+    const std::set<std::string> GetBusesByStop(std::string_view stop_name) const;
+
+    bool IsBusNumber(const std::string_view bus_number) const;
+    bool IsStopName(const std::string_view stop_name) const;
+
+    svg::Document RenderMap() const;
+
+private:
+    const transport::TransportCatalogue& catalogue_;
+    const renderer::MapRenderer& renderer_;
+};
