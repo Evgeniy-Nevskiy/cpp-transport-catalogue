a776e2f7d36da3790773f98a059c5a483d7e4edb
Evgeniy-Nevskiy <122367665+Evgeniy-Nevskiy@users.noreply.github.com>
2023-08-07 01:48:51 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -17,20 +17,15 @@ public:
     const json::Node& GetStatRequests() const;
     const json::Node& GetRenderSettings() const;
 
-    void ProcessRequests(const json::Node& stat_requests, RequestHandler& rh) const;
-
     void FillCatalogue(transport::TransportCatalogue& catalogue);
-    renderer::MapRenderer FillRenderSettings(const json::Dict& request_map) const;
+    renderer::MapRenderer FillRenderSettings(const json::Map& request_map) const;
 
-    const json::Node ShowBus(const json::Dict& request_map, RequestHandler& rh) const;
-    const json::Node ShowStop(const json::Dict& request_map, RequestHandler& rh) const;
-    const json::Node ShowMap(const json::Dict& request_map, RequestHandler& rh) const;
 
 private:
     json::Document input_;
     json::Node dummy_ = nullptr;
 
-    std::tuple<std::string_view, geo::Coordinates, std::map<std::string_view, int>> FillStop(const json::Dict& request_map) const;
+    std::tuple<std::string_view, geo::Coordinates, std::map<std::string_view, int>> FillStop(const json::Map& request_map) const;
     void FillStopDistances(transport::TransportCatalogue& catalogue) const;
-    std::tuple<std::string_view, std::vector<const transport::Stop*>, bool> FillBus(const json::Dict& request_map, transport::TransportCatalogue& catalogue) const;
+    std::tuple<std::string_view, std::vector<const transport::Stop*>, bool> FillBus(const json::Map& request_map, transport::TransportCatalogue& catalogue) const;
 };
